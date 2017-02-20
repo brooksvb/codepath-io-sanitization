d8843f4204ce651f25a724b1b7053f5a70585671
Brooks Van Buren <woofiewilly@gmail.com>
2017-02-20 11:05:17 -0500

    Fixed initialize.php reference (different directory level)

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php require_once('../../private/initialize.php'); ?>
+<?php require_once('./../private/initialize.php'); ?>
 
 <?php $page_title = 'Globitek CMS: Menu'; ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
