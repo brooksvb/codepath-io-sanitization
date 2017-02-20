e3ee8a3de918fc548c32d1a0f9a898b19c77dd78
Brooks Van Buren <woofiewilly@gmail.com>
2017-02-20 11:04:31 -0500

    Added index.php in top-level folder

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php $page_title = 'Globitek CMS: Menu'; ?>
+<?php include(SHARED_PATH . '/header.php'); ?>
+
+<div id="main-content">
+  <h1>Menu</h1>
+  <ul>
+    <li>
+      <a href="staff/index.php">Staff</a>
+    </li>
+    <li>
+      <a href="register.php">Register</a>
+    </li>
+    <li>
+      <a href="territories.php">Territories</a>
+    </li>
+    <li>
+      <a href="salesperson.php">Salesperson</a>
+    </li>
+  </ul>
+</div>
+
+<?php include(SHARED_PATH . '/footer.php'); ?>
